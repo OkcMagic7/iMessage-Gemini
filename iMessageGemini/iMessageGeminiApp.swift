9c3f73290a499bd3f700005a4c16915a8dfe58bd
Hunter Ward <hw.magic@icloud.com>
2024-01-27 04:12:14 -0600

    Initial Commit

---
+++ iMessageGemini/iMessageGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageGeminiApp.swift
+//  iMessageGemini
+//
+//  Created by Hunter Ward on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct iMessageGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
